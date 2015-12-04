94d73e4e981ebb17c8f8976e9d3abae3c6c0b8dd
GAVSHARAS <sarunas.gav@gmail.com>
2015-12-04 23:10:39 +0200

    Dar vienas bandymas

---
+++ lsn02.rb
@@ -0,0 +1,20 @@
+#Strings
+
+my_string = "Hello world!"
+
+name = "Sharas"
+
+greeting = "Hello, my name is #{name}"
+
+puts greeting
+puts my_string
+
+addition = "25 x 8 = #{25 * 8}"
+
+puts addition
+
+def name.shout
+    "#{self.upcase}!!!"
+end
+
+puts name.shout    
